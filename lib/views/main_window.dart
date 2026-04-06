587cbb0ea3118d4265d1c8b94b390e3e0ca82161
icd360sevofficial <github@icd360s.de>
2026-04-06 22:26:23 +0000

    release(2.18.0): bump version in main_window.dart

--- lib/views/main_window.dart
+++ lib/views/main_window.dart
@@ -1336,7 +1336,7 @@ class _MainWindowState extends State<MainWindow> {
                 onPressed: _showChangelog,
                 builder: (context, states) {
                   return Text(
-                    l10n.mainWindowVersion('2.17.11'),
+                    l10n.mainWindowVersion('2.18.0'),
                     style: theme.typography.caption?.copyWith(
                       color: states.isHovered ? theme.accentColor.light : theme.accentColor,
                       fontWeight: FontWeight.bold,
