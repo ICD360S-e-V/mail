08c439beccc643ab7c121bb3234e34e20a3e7c7b
icd360sevofficial <github@icd360s.de>
2026-04-24 10:09:55 +0000

    chore: add SPDX license header

--- lib/utils/safe_filename.dart
+++ lib/utils/safe_filename.dart
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024-2026 ICD360S e.V.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'package:path/path.dart' as p;
 
 /// Sanitize an attachment filename for safe local storage.
