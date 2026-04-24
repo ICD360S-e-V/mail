3c7a59e36dc9721daa0591d049f8a70f6ae39e86
icd360sevofficial <github@icd360s.de>
2026-04-24 10:09:20 +0000

    chore: add SPDX license header

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024-2026 ICD360S e.V.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'dart:io';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import '../models/models.dart';
