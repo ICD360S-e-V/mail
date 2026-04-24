9268f68c7d9eaefa616f7f2f686797afb5b4998f
icd360sevofficial <github@icd360s.de>
2026-04-24 10:08:00 +0000

    chore: add SPDX license header

--- lib/services/certificate_expiry_monitor.dart
+++ lib/services/certificate_expiry_monitor.dart
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024-2026 ICD360S e.V.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'package:basic_utils/basic_utils.dart';
 import 'certificate_service.dart';
 import 'logger_service.dart';
