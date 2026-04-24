e36a6dc96ea3ccd1fba913a0c5d52e4e7f407211
icd360sevofficial <github@icd360s.de>
2026-04-24 10:08:05 +0000

    chore: add SPDX license header

--- lib/services/changelog_service.dart
+++ lib/services/changelog_service.dart
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024-2026 ICD360S e.V.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'dart:convert';
 import 'dart:io';
 import 'le_issuer_check.dart';
@@ -62,4 +65,3 @@ class ChangelogService {
     return null;
   }
 }
-
